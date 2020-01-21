fcf1ce69fe80116837557d9f6c27ebacc0a4c00a
Jean van Niekerk <jean.jacque@rocketmail.com>
2020-01-21 14:24:22 +0200

    Client & Dashboard component

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -10,12 +10,21 @@ import { Client } from "../../models/Client";
 })
 export class ClientsComponent implements OnInit {
   clients: Client[];
+  totalOwed: number;
 
   constructor(private clientService: ClientService) {}
 
   ngOnInit() {
-    this.clientService
-      .getClients()
-      .subscribe(clients => (this.clients = clients));
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    });
+  }
+
+  getTotalOwed() {
+    const total = this.clients.reduce((total, client) => {
+      return total + client.balance;
+    }, 0);
+    this.totalOwed = total;
   }
 }
